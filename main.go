18800c60119f2dc599409d2d2e7f239875fab915
tarifalhasan <tarifalhasan999@gmail.com>
2024-10-28 15:51:24 +0600

    variable

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var name ,country string
+
+	var age int
+	var gpa float32
+
+	name = "Tarif Al Hasan"
+	country = "Bangladesh"
+	age = 20
+	gpa = 4.44
+	
+	fmt.Println(name,"is my name","I'm ",age,"years old my country name is",country,"my gpa is",gpa )
+
+  
+
+}
